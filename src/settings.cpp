77a2cec74603e06f4dde5d6f94d0ec320601bba8
LumadevVT <lumadev.vt@gmail.com>
2026-01-29 11:38:43 -0300

    Add Try-Catch to handle opening settings file.

--- src/settings.cpp
+++ src/settings.cpp
@@ -20,7 +20,6 @@ namespace Settings
         //     size_t downloadPathSize = 0;
         //     const char *downloadPath = nullptr;
         // };
-
         struct Preferences
         {
             fs::path path;
@@ -37,82 +36,87 @@ namespace Settings
         char* prefPath = SDL_GetPrefPath(ORGANIZATION, APP_NAME);
         if (prefPath == nullptr)
         {
-            Log::Warn("Could not load preferences folder!");
             fs::create_directory(fs::temp_directory_path() / "SentYa");
             prefs.path = fs::temp_directory_path() / "SentYa";
         }
-        else prefs.path = prefPath;
-        SDL_free(prefPath);
+        else
+        {
+            prefs.path.assign(prefPath);
+
+            SDL_free(prefPath);
+        }
 
         const char* downloadFolder = SDL_GetUserFolder(SDL_FOLDER_DOWNLOADS);
-        if (downloadFolder == nullptr)
+        if (downloadFolder != nullptr)
         {
-            Log::Warn("Could not load download folder!");
-            fs::create_directory(fs::temp_directory_path() / "SentYa" / "Downloads");
-            prefs.downloadFolder = fs::temp_directory_path() / "SentYa" / "Downloads";
+            prefs.downloadFolder = fs::path(downloadFolder);
+            return;
         }
-        else prefs.downloadFolder = downloadFolder;
+
+        Log::Warn("Could not load download folder!");
+        fs::create_directory(fs::temp_directory_path() / "SentYa" / "Downloads");
+        prefs.downloadFolder = fs::temp_directory_path() / "SentYa" / "Downloads";
     }
 
 
     bool LoadPrefsFromDisk()
     {
-        const char* prefPath = SDL_GetPrefPath(ORGANIZATION, APP_NAME);
+        char* prefPath = SDL_GetPrefPath(ORGANIZATION, APP_NAME);
         if (prefPath == nullptr)
         {
             Log::Warn("Could not load preferences path from SDL!");
             return false;
         }
+        prefs.path.assign(prefPath);
+        SDL_free(prefPath);
+
+        std::ifstream preferencesFile;
+        preferencesFile.open(prefs.path / SETTINGS_FILENAME);
+        if (!preferencesFile.is_open())
+        {
+            Log::Warn("Could not open preferences file!");
+            return false;
+        }
 
         try
         {
-            std::ifstream preferencesFile(fs::path(prefPath) / SETTINGS_FILENAME);
             json data = json::parse(preferencesFile);
+            Log::Debug(data.dump(4));
+            prefs.downloadFolder = data.at("downloadFolder").get<std::string>();
 
-            prefs = {
-                .path = prefPath,
-                .downloadFolder = data["downloadFolder"].get<std::string>(),
-            };
+            preferencesFile.close();
             return true;
         }
         catch (const std::exception& e)
         {
-            Log::Error(e.what());
+            Log::Error(std::string("Error loading json from preferences: ") + e.what());
+            preferencesFile.close();
             return false;
         }
     }
 
     bool Init()
     {
-        if (!LoadPrefsFromDisk()) LoadDefaultPreferences();
-
+        if (!LoadPrefsFromDisk())
+            LoadDefaultPreferences();
         return true;
     }
 
     void SaveToDisk()
     {
+        Log::Debug((prefs.path / SETTINGS_FILENAME).string());
+
         std::ofstream preferencesFile;
         preferencesFile.open(prefs.path / SETTINGS_FILENAME);
         if (!preferencesFile.is_open()) return;
 
+
         json jsonData;
-        jsonData["downloadFolder"] = prefs.downloadFolder.native();
+        jsonData.emplace("downloadFolder", prefs.downloadFolder);
 
         preferencesFile << std::setw(4) << jsonData << std::endl;
+        preferencesFile.close();
     }
-
-
-    // TODO: Maybe save preferences for SentYa in future updates
-    // void SaveToDisk() {
-    //     static_assert(std::is_standard_layout_v<SavePreferences> == true);
-    //     std::string filePath = prefsPath.append("SentYa.config").string();
-    //
-    //     SavePreferences savePrefs = {};
-    //     savePrefs.downloadPath = prefs.downloadPath.native().c_str();
-    //     savePrefs.downloadPathSize = prefs.downloadPath.native().length();
-    //
-    //     SDL_SaveFile(filePath.c_str(), &savePrefs, sizeof(savePrefs));
-    // }
 
     fs::path& GetDownloadFolderPath()
     {
