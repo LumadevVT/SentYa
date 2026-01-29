ecdb31cbe44a7458d249ba58e17deda438c47b4d
LumadevVT <lumadev.vt@gmail.com>
2026-01-29 11:37:57 -0300

    Change custom logging system to use SDL_Log.h

--- src/utils/log.cpp
+++ src/utils/log.cpp
@@ -5,32 +5,53 @@
 #include <filesystem>
 
 #include "globals.h"
+#include "SDL3/SDL_log.h"
 #include "SDL3/SDL_messagebox.h"
 
 namespace fs = std::filesystem;
 
-namespace Log {
-	namespace {
-		LogLevel currentLevel = LogLevel::Error;
+namespace Log
+{
+	namespace
+	{
 		std::ofstream logFile;
 	}
 
-	void Init() {
-		logFile.open(fs::current_path() / "log.txt");
-		if (!logFile.is_open()) {
-			std::exit(EXIT_FAILURE);
+	static void LogOutputCallback(void*, int category, SDL_LogPriority priority, const char* message)
+	{
+		switch (priority)
+		{
+		// case SDL_LOG_PRIORITY_CRITICAL:
+		//
+		// case SDL_LOG_PRIORITY_ERROR:
+		// case SDL_LOG_PRIORITY_WARN:
+		// case SDL_LOG_PRIORITY_INFO:
+		default:
+			break;
 		}
 	}
 
-	void Quit() {
+	void Init()
+	{
+		logFile.open(fs::current_path() / "log.txt");
+		if (!logFile.is_open())
+		{
+			std::exit(EXIT_FAILURE);
+		}
+		// SDL_SetLogOutputFunction(LogOutputCallback, nullptr);
+	}
+
+	void Quit()
+	{
 		logFile.close();
 	}
 
-	void SetLogLevel(const LogLevel level) {
-		currentLevel = level;
+	void SetLogLevel(const LogLevel level)
+	{
 	}
 
-	void ShowFatalMessageBox(const std::string &message) {
+	void ShowFatalMessageBox(const std::string& message)
+	{
 		SDL_ShowSimpleMessageBox(
 			SDL_MESSAGEBOX_ERROR,
 			"A fatal error has occurred!",
@@ -39,50 +60,24 @@ namespace Log {
 		);
 	}
 
-	void Message(const LogLevel level, const std::string &message) {
-#ifdef DEBUG
-		std::string finalMessage;
-		if (level >= LogLevel::Debug) {
-			if (currentLevel < level) return;
-
-			finalMessage += "[DEBUG]: " + message;
-		} else if (level >= LogLevel::Warn) {
-			if (currentLevel < level) return;
-
-			finalMessage += "[WARNING]: " + message;
-		} else if (level >= LogLevel::Error) {
-			if (currentLevel < level) return;
-			finalMessage += "[ERROR]: " + message;
-		} else if (level >= LogLevel::Fatal) {
-			if (currentLevel < level) return;
-			finalMessage += "[FATAL]: " + message;
-		}
-
-		if (logFile.is_open()) {
-			logFile << finalMessage;
-		}
-
-		if (level < LogLevel::Error)
-			std::cout << finalMessage << std::endl;
-		else
-			std::cerr << finalMessage << std::endl;
-#endif
+	void Debug(const std::string& message)
+	{
+		SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "%s", message.c_str());
 	}
 
-	void Debug(const std::string &message) {
-		Message(LogLevel::Debug, message);
+	void Warn(const std::string& message)
+	{
+		SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, "%s", message.c_str());
 	}
 
-	void Warn(const std::string &message) {
-		Message(LogLevel::Warn, message);
+	void Error(const std::string& message)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s", message.c_str());
 	}
 
-	void Error(const std::string &message) {
-		Message(LogLevel::Error, message);
-	}
-
-	void Fatal(const std::string &message) {
-		Message(LogLevel::Fatal, message);
+	void Fatal(const std::string& message)
+	{
+		SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "%s", message.c_str());
 		ShowFatalMessageBox(message);
 	}
 }
