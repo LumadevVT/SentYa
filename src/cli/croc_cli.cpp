0096f9e85b0f259cc778aaa16a0548c76a49c41a
LumadevVT <lumadev.vt@gmail.com>
2026-01-29 16:17:25 -0300

    Revert last stdout process property

--- src/cli/croc_cli.cpp
+++ src/cli/croc_cli.cpp
@@ -144,7 +144,7 @@ Process CrocCLI::ReceiveArchive(const std::string& ticket)
     SDL_SetEnvironmentVariable(env, "CROC_SECRET", newProcess.ticket.c_str(), true);
     SDL_SetPointerProperty(props, SDL_PROP_PROCESS_CREATE_ENVIRONMENT_POINTER, env);
     SDL_SetNumberProperty(props, SDL_PROP_PROCESS_CREATE_STDIN_NUMBER, SDL_PROCESS_STDIO_NULL);
-    SDL_SetNumberProperty(props, SDL_PROP_PROCESS_CREATE_STDOUT_NUMBER, SDL_PROCESS_STDIO_APP);
+    SDL_SetNumberProperty(props, SDL_PROP_PROCESS_CREATE_STDOUT_NUMBER, SDL_PROCESS_STDIO_INHERITED);
     newProcess.sdlProcess = SDL_CreateProcessWithProperties(props);
     if (newProcess.sdlProcess == nullptr)
     {
