5ed22a98475060179f9204af0beba9b51b78afa9
LumadevVT <lumadev.vt@gmail.com>
2026-01-29 13:20:23 -0300

    Fix CMake option to download correct binaries.

--- cmake/GetBinaries.cmake
+++ cmake/GetBinaries.cmake
@@ -1,12 +1,12 @@
 include(FetchContent)
 
 option(USE_CROC_BACKEND "Use croc backend for SentYa" ON)
-option(USE_SENDME_BACKEND "Use sendme backend for SentYa" ON)
+option(USE_SENDME_BACKEND "Use sendme backend for SentYa" OFF)
 
 if (USE_CROC_BACKEND)
-    include(cmake/DownloadSendme.cmake)
+    include(cmake/DownloadCroc.cmake)
 endif ()
 
 if (USE_SENDME_BACKEND)
-    include(cmake/DownloadCroc.cmake)
+    include(cmake/DownloadSendme.cmake)
 endif ()
